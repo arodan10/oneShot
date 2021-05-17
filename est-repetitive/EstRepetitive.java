ffaf928bb27cd068b3b4d999b9e3f0afba5d8e08
arodan10 <950315128arodan@gmail.com>
2021-05-17 15:01:06 -0500

    repetitive

--- est-repetitive/EstRepetitive.java
+++ est-repetitive/EstRepetitive.java
@@ -1,82 +1,99 @@
 import java.util.Scanner;
 
 /**
- * EstRepetitive
+ * EstRepetitive hace comentario un bloque de codigo
  */
 //comenta una sola linea
 public class EstRepetitive {
-    //objeto de un teclado
-    static Scanner objTeclado=new Scanner(System.in);//Permite leer varibles
-    /*Inicio a, b son atributos o varibles, pero varibles glovales*/ 
+    //objTeclado es un objeto
+    static Scanner objTeclado=new Scanner(System.in);//Permite leer valores por teclado
+    /* Inicio a, b son atributos o Variables, pero variables globales*/
     static int a=10;
-    static double b=5;
-    /*Fin  a, b son atributos o varibles, pero varibles glovales*/
+    double b=5;
+    /* Fin a, b son atributos o Variables, pero variables globales*/
 
-    /*Estos son metodos*/    
+    /* Estos son metodos */
     public static void saludo() {
-        System.out.print("Ingrese su Nombre:"); 
-        String nombre=objTeclado.next();  //leer nombre
-        System.out.println("Hola: "+nombre+ " ¿Como estas?");   
-        System.out.println("suma:"+ (a+b) );   
-    } 
+        System.out.print("Ingrese su nombre:"); 
+        String nombre=objTeclado.next();//Leer nombre
+        System.out.println("Hola: "+nombre+" Como estas?");         
+        System.out.println("Suma:"+ (a) );
+    }
 
     public void saludo2() {
-        System.out.print("Ingrese su Nombre:"); 
-        String nombre=objTeclado.next(); //leer nombre
-        System.out.println("Hola: "+nombre+ " ¿Como estas?");      
-    }
-    
-    public static void suma10numerosmientras() {
-        //declarar variables
+        System.out.print("Ingrese su nombre:"); 
+        String nombre=objTeclado.next();//Leer nombre        
+        System.out.println("Hola: "+nombre+" Como estas?");  
+        System.out.println("Suma:"+ (a+b) );       
+    }    
+
+    public static void suma10NumerosMientras() {
+        //Declarar Variables
         double numeros, sumaNumeros=0;
         int contador=1;
-        //Datos de Entrada y proceso
+        //Datos de entrada y Proceso
         while(contador<=10){
-            System.out.println("Ingrese el valor de la posiscion "+contador+":");
+            System.out.println("Ingrese el valor de la posicion "+contador+":");
             numeros=objTeclado.nextDouble();
             sumaNumeros=sumaNumeros+numeros;
-            contador++;
-
+            contador++; //contador=contador+1;
         }
-        System.out.println("la suma delos 10 numeros es:"+sumaNumeros);
-        
+        System.out.println("La suma de los 10 numeros es:"+sumaNumeros);
     }
 
-    public static void suma10numeroshacermientras() {
-        //declarar variables
+    public static void suma10NumerosHacerMientras() {
+        //Declarar Variables
         double numeros, sumaNumeros=0;
         int contador=1;
-        //Datos de Entrada y proceso
-        do{
-            System.out.println("Ingrese el valor de la posiscion "+contador+":");
+        //Datos de entrada y Proceso
+        do{        
+            System.out.println("Ingrese el valor de la posicion "+contador+":");
             numeros=objTeclado.nextDouble();
-            sumaNumeros=sumaNumeros+numeros;
-            contador++;
+            sumaNumeros=sumaNumeros+numeros;//29
+            contador++;    //contador=contador+1                    
         }
         while(contador<=10);
-        System.out.println("la suma delos 10 numeros es:"+sumaNumeros);
-        
+        System.out.println("La suma de los 10 numeros es:"+sumaNumeros);
     }
 
-    public static void suma10numerospara() {
-        //declarar variables
-        double numeros, sumaNumeros=0;
-        
-        //Datos de Entrada y proceso
-        for(int contador=1;contador<=10;contador++){
-            System.out.println("Ingrese el valor de la posiscion "+contador+":");
+
+    public static void suma10NumerosPara() {
+        //Declarar Variables
+        double numeros, sumaNumeros=0;        
+        //Datos de entrada y Proceso
+        for(int contador=1;contador<=10;contador++){        
+            System.out.println("Ingrese el valor de la posicion "+contador+":");
             numeros=objTeclado.nextDouble();
-            sumaNumeros=sumaNumeros+numeros;
-            
-        }        
-        System.out.println("la suma delos 10 numeros es:"+sumaNumeros);
-        
+            sumaNumeros=sumaNumeros+numeros;                                    
+        }
+        System.out.println("La suma de los 10 numeros es:"+sumaNumeros);
+    }    
+
+    public static void menuMain() {
+        String mensaje="Seleccion el algoritmo que desea probar:"+
+        "\n1=suma10NumerosMientras"+
+        "\n2=suma10NumerosHacerMientras"+
+        "\n3=suma10NumerosPara"+
+        "\n0=Salir del sistema"
+        ;
+        System.out.println(mensaje);
+        int opciones=objTeclado.nextInt();         
+        do {
+            switch (opciones) {
+                case 1: suma10NumerosMientras(); break;
+                case 2: suma10NumerosHacerMientras(); break;
+                case 3: suma10NumerosPara(); break;        
+                default:System.out.println("Opcion no existe");   break;
+            }            
+            if (opciones!=0){ 
+            System.out.println("Desea probar otraves? "+mensaje);
+            opciones=objTeclado.nextInt(); 
+            }            
+        }while (opciones!=0);
     }
 
-    public static void main(String[] args) {
-      //suma10numeros();
-      //suma10numeroshacermientras();
-      suma10numerospara();
+    public static void main(String[] args) { 
+        menuMain();
     }
-    
+      
 }
