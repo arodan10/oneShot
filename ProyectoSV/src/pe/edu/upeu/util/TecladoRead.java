d135d165a5446adf7f38261121ab9591658f8668
arodan10 <>
2021-06-03 14:42:49 +0000

    lol

--- ProyectoSV/src/pe/edu/upeu/util/TecladoRead.java
+++ ProyectoSV/src/pe/edu/upeu/util/TecladoRead.java
@@ -2,70 +2,64 @@ package pe.edu.upeu.util;
 
 import java.io.*;
 
-
-
 public class TecladoRead {
 
-BufferedReader readerI = new BufferedReader(new InputStreamReader(System.in));
+BufferedReader readerI =new BufferedReader(new InputStreamReader(System.in));  
 
-public int leer(int valor, String msg){
+public int read(int valor, String msg) {
     System.out.println(msg);
     try {
-        valor=Integer.parseInt(readerI.readLine());        
+        valor=Integer.parseInt(readerI.readLine());
     } catch (Exception e) {
         System.out.println("Intente de nuevo");
-        valor=leer(valor,msg);
-    }
-    return valor;
-
-}
-
-public double leer(double valor, String msg){
-    System.out.println(msg);
-    try {
-        valor=Double.parseDouble(readerI.readLine());        
-    } catch (Exception e) {
-        System.out.println("Intente de nuevo");
-        valor=leer(valor,msg);
-    }
-    return valor;
-    
-}
-
-public char leer(char valor, String msg){
-    System.out.println(msg);
-    try {
-        valor=readerI.readLine().charAt(0);        
-    } catch (Exception e) {
-        System.out.println("Intente de nuevo");
-        valor=leer(valor,msg);
+        valor=read(valor,msg);
     }
     return valor;
 }
 
-public String leer(String valor, String msg){
+public double read(double valor, String msg) {
     System.out.println(msg);
     try {
-        valor=readerI.readLine();        
+        valor=Double.parseDouble(readerI.readLine());
     } catch (Exception e) {
         System.out.println("Intente de nuevo");
-        valor=leer(valor,msg);
+        valor=read(valor,msg);
     }
-    return valor;
+    return valor;    
 }
 
-public boolean leer(boolean valor, String msg){
+public char read(char valor, String msg) {
     System.out.println(msg);
     try {
-        valor=Boolean.parseBoolean(readerI.readLine());        
+        valor=readerI.readLine().charAt(0);
     } catch (Exception e) {
         System.out.println("Intente de nuevo");
-        valor=leer(valor,msg);
+        valor=read(valor,msg);
     }
-    return valor;
-    
+    return valor;    
+}
+
+public String read(String valor, String msg) {
+    System.out.println(msg);
+    try {
+        valor=readerI.readLine();
+    } catch (Exception e) {
+        System.out.println("Intente de nuevo");
+        valor=read(valor,msg);
+    }
+    return valor;      
+}
+
+public boolean read(boolean valor, String msg) {
+    System.out.println(msg);
+    try {
+        valor=Boolean.parseBoolean(readerI.readLine());
+    } catch (Exception e) {
+        System.out.println("Intente de nuevo");
+        valor=read(valor,msg);
+    }
+    return valor;        
 }
 
 
-    
 }
