c55f472418e11e8b00fb14f942cd3ac67c4e64a5
arodan10 <950315128arodan@gmail.com>
2021-06-17 08:45:12 -0500

    lol

---
+++ ProyectoVentasG1/src/main/java/pe/edu/upeu/gui/MainGUI.java
@@ -0,0 +1,43 @@
+package pe.edu.upeu.gui;
+
+import javax.swing.JFrame;
+import pe.edu.upeu.util.UtilsX;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+public class MainGUI extends JFrame implements ActionListener{
+
+    private static final long serialVersionUID = 1L;
+int numeros;
+JPanel panel;
+JTextField texto;
+JScrollPane scrollPane=new JScrollPane();
+BufferedImage image;
+JTable table;
+JMenuBar mb;
+JMenu m1;
+JMenu m2;
+JMenuItem m11;
+JMenuItem m22;
+UtilsX obj=new UtilsX();
+JPanel panelFoot;
+JButton send, reset;
+
+    public MainGUI(){
+        this.setTitle("SystemMain@DMP");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setSize(new Dimension(screenSize.width, screenSize.height-3
+
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
