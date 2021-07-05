c71cb5a77c1e0d9300e7ef61789c4875e02c8136
arodan10 <82171083+arodan10@users.noreply.github.com>
2021-07-05 12:49:12 -0500

    lol

--- ProyectoVentasG1/src/main/java/pe/edu/upeu/app/App.java
+++ ProyectoVentasG1/src/main/java/pe/edu/upeu/app/App.java
@@ -1,15 +1,65 @@
 package pe.edu.upeu.app;
 
+import pe.edu.upeu.dao.CategoriaDAO;
+import pe.edu.upeu.dao.ProductoDAO;
+import pe.edu.upeu.dao.UsuarioDAO;
 import pe.edu.upeu.gui.MainGUI;
+import pe.edu.upeu.util.TecladoRead;
 
 /**
  * Hello world!
  *
  */
 public class App{
+
+    public static void menuMain() {
+        String mensaje="Seleccion el algoritmo que desea probar:"+
+        "\n1=Registrar Categoria"+
+        "\n11=Reportar Categoria"+
+        "\n2=Registrar Producto"+
+        "\n3=Registrar Usuario"+
+        "\n4=Registrar Venta"+
+        "\n0=Salir del sistema"
+        ;     
+        CategoriaDAO daoCat;   
+        UsuarioDAO daoUso;
+        ProductoDAO proDao;
+        TecladoRead tre=new TecladoRead();
+        int opciones=tre.read(0, mensaje);         
+        do {
+            switch (opciones) {
+                case 1: daoCat=new CategoriaDAO(); daoCat.registrarCategoria(); break;
+                case 11: daoCat=new CategoriaDAO(); daoCat.reporteCategoria(); break;
+                case 2: proDao=new ProductoDAO(); proDao.registrarProducto(); break;
+                case 3: daoUso=new UsuarioDAO(); daoUso.registrarUsuario();  break;        
+                default:System.out.println("Opcion no existe");   break;
+            }            
+            if (opciones!=0){ 
+            System.out.println("Desea probar otraves? ");
+            opciones=tre.read(0, mensaje);
+            }            
+        }while (opciones!=0);
+    }
+
+    public static void validAccessSystem() {
+        System.out.println("******************* Sistema de Ventas ******************");
+        TecladoRead tr=new TecladoRead();
+        UsuarioDAO uDao=new UsuarioDAO();
+        String usuario=tr.read("", "Ingrese su Usuario:");
+        System.out.println("Ingres su clave:");
+        char[] clave=System.console().readPassword();
+        if (uDao.login(usuario,clave)) {
+            menuMain();
+        } else {
+            System.out.println("Error de autentifiacion.......Intetente nuevamente!!");
+            validAccessSystem();
+        }
+    }
+
+
     public static void main( String[] args ){
-        System.out.println( "Hello World!" );
-        new MainGUI();
+        //menuMain();
+        validAccessSystem();
+        //new MainGUI();
     }
 }
-
---
+++ ProyectoVentasG1/src/main/java/pe/edu/upeu/modelo/CategoriaTO.java
@@ -0,0 +1,24 @@
+package pe.edu.upeu.modelo;
+
+public class CategoriaTO {
+
+public String idCaget, nombre;
+
+public String getIdCaget() {
+    return idCaget;
+}
+
+public void setIdCaget(String idCaget) {
+    this.idCaget = idCaget;
+}
+
+public String getNombre() {
+    return nombre;
+}
+
+public void setNombre(String nombre) {
+    this.nombre = nombre;
+}
+
+
+}
